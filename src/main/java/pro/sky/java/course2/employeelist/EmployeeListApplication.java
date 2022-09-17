a8c0bfa7811a406e798087424742e618da0b88ff
danya1705 <danya17@gmail.com>
2022-09-17 10:25:38 +0300

    first commit

---
+++ src/main/java/pro/sky/java/course2/employeelist/EmployeeListApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeListApplication.class, args);
+    }
+
+}
